eb84389eb534dae8ab593fd00008e1aba691bba0
haizai007 <haizai007@gmail.com>
2026-03-20 09:44:43 +0800

    build: enforce 'edge' runtime to comply with Cloudflare Pages execution bounds

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -5,6 +5,8 @@ import { logout } from "@/app/login/actions";
 import Link from "next/link";
 import "./globals.css";
 
+export const runtime = 'edge';
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
